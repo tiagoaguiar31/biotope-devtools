5d14e8e1c3897480056a5fdc47889d08f85c33e6
Tiago Aguiar <tiago.aguiar@virtual-identity.com>
2019-07-29 09:48:26 +0100

    right click icon showing

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,20 +1,33 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
+//All context Menus gets stored in here
+let contextMenus = {};
 
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-  
-});
+contextMenus.biotopeDevtools = 
+  chrome.contextMenus.create(
+    {"title": "Biotope DevTools"},
+    function() {
+      if(chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError.message);
+      }
+    }
+    );
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+// chrome.runtime.onInstalled.addListener(details => {
+//   console.log('previousVersion', details.previousVersion);
+
+//   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+//     chrome.declarativeContent.onPageChanged.addRules([{
+//       conditions: [new chrome.declarativeContent.PageStateMatcher({
+//         pageUrl: {hostEquals: 'developer.chrome.com'},
+//       })
+//       ],
+//           actions: [new chrome.declarativeContent.ShowPageAction()]
+//     }]);
+//   });
+
+// });
+
+// chrome.browserAction.setBadgeText({text: '\'Allo'});
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
